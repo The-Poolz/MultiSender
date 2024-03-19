2dfdc861835ffb9d0b7d661aa89dd15a3dfbc872
Ashwin Arora <ashwinarora48@gmail.com>
2024-03-20 00:25:20 +0530

    added direct and indiect methods

--- contracts/MultiSender.sol
+++ contracts/MultiSender.sol
@@ -51,7 +51,7 @@ contract MultiSender is MultiManageable {
         emit MultiTransferredETH(_multiSendData.length, msg.value - fee);
     }
 
-    function MultiSendERC20(
+    function MultiSendERC20Indirect(
         address _token,
         uint256 _totalAmount,
         MultiSendData[] calldata _multiSendData
@@ -68,6 +68,34 @@ contract MultiSender is MultiManageable {
         IERC20(_token).transferFrom(msg.sender, address(this), _totalAmount);
         for (uint256 i; i < _multiSendData.length; i++) {
             IERC20(_token).transfer(_multiSendData[i].user, _multiSendData[i].amount);
+        }
+        uint256 remaining = IERC20(_token).balanceOf(address(this));
+        if(remaining != 0) {
+            IERC20(_token).transfer(msg.sender, remaining);
+        }
+        emit MultiTransferredERC20(
+            _token,
+            _multiSendData.length,
+            _totalAmount
+        );
+    }
+    
+    function MultiSendERC20Direct(
+        address _token,
+        uint256 _totalAmount,
+        MultiSendData[] calldata _multiSendData
+    )
+        public
+        payable
+        whenNotPaused
+        notZeroLength(_multiSendData.length)
+    {
+        require(_token != address(0), "Invalid token address");
+        uint256 fee = _calcFee();
+        PayFee(fee);
+        if (FeeToken == address(0) && msg.value != fee) revert FeeNotProvided(fee);
+        for (uint256 i; i < _multiSendData.length; i++) {
+            IERC20(_token).transferFrom(msg.sender, _multiSendData[i].user, _multiSendData[i].amount);
         }
         uint256 remaining = IERC20(_token).balanceOf(address(this));
         if(remaining != 0) {
