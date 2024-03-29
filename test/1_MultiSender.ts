c28578ee5bf13fac546463ac486b3349d1b5c9a0
Ashwin Arora <ashwinarora48@gmail.com>
2024-03-29 14:15:57 +0530

    added tests and refactor

--- test/1_MultiSender.ts
+++ test/1_MultiSender.ts
@@ -1,19 +1,15 @@
 import { ethers } from "hardhat"
-import { ERC20, MultiSender } from "../typechain-types"
-import { assert } from "console"
+import { ERC20, MultiSenderV2 } from "../typechain-types"
 import { expect } from "chai"
-import { BigNumber } from "bignumber.js"
-
 const ZERO_ADDRESS: string = '0x0000000000000000000000000000000000000000';
 
-describe("MultiSender", () => {
-    let instance: MultiSender
+describe("MultiSenderV2", () => {
+    let instance: MultiSenderV2
     let token: ERC20
     const amount = 1000
-    const amounts = Array(10).fill(amount)
 
     beforeEach(async () => {
-        instance = await ethers.deployContract("MultiSender")
+        instance = await ethers.deployContract("MultiSenderV2")
         token = await ethers.deployContract("ERC20Token", ["TestToken", "TEST"])
     })
 
@@ -33,7 +29,7 @@ describe("MultiSender", () => {
         }
     });
 
-    it("should transfer ERC20 tokens to multiple accounts", async function () {
+    it("should transfer ERC20 tokens to multiple accounts directly", async function () {
         const [deployer, ...accounts] = await ethers.getSigners();    
         const amount = ethers.parseUnits("1", 18); 
         await token.connect(deployer).approve(instance.getAddress(), amount * BigInt(accounts.length) );
@@ -42,7 +38,7 @@ describe("MultiSender", () => {
             amount: amount,
         }));
         const total = users.reduce((acc, user) => acc + user.amount, 0n);
-        await instance.connect(deployer).MultiSendERC20Direct(token.getAddress(), total, users );
+        await instance.connect(deployer).MultiSendERC20Direct(token.getAddress(), users );
     
         for (let account of accounts) {
           let bal = await token.balanceOf(account.address);
@@ -50,11 +46,11 @@ describe("MultiSender", () => {
         }
     });
 
-    it("should transfer ERC20 tokens to multiple accounts", async function () {
+    it("should transfer ERC20 tokens to multiple accounts indirectly", async function () {
         const [deployer, ...accounts] = await ethers.getSigners();    
         const amount = ethers.parseUnits("1", 18); 
         await token.connect(deployer).approve(instance.getAddress(), amount * BigInt(accounts.length) );
-        const users = accounts.map((acc, index) => ({
+        const users = accounts.map((acc) => ({
             user: acc.address,
             amount: amount,
         }));
@@ -70,15 +66,40 @@ describe("MultiSender", () => {
     describe("Revert Tests", () => {
 
         it("should revert zero address erc transfer", async () => {
-            await expect(instance.MultiSendERC20Direct(ZERO_ADDRESS, 0, [])).to.be.revertedWithCustomError(instance, "InvalidTokenAddress")
+            await expect(instance.MultiSendERC20Direct(ZERO_ADDRESS, [])).to.be.revertedWithCustomError(instance, "InvalidTokenAddress")
         })
 
 
         it("should revert zero length array", async () => {
             await expect(instance.MultiSendEth([])).to.be.revertedWithCustomError(instance, "ArrayZeroLength")
-            await expect(instance.MultiSendERC20Direct(token.getAddress(), 0, [])).to.be.revertedWithCustomError(instance, "ArrayZeroLength")
+            await expect(instance.MultiSendERC20Direct(token.getAddress(), [])).to.be.revertedWithCustomError(instance, "ArrayZeroLength")
             await expect(instance.MultiSendERC20Indirect(token.getAddress(), 0, [])).to.be.revertedWithCustomError(instance, "ArrayZeroLength")
         })
+
+        it("should revert ERC20 indirect transfer when total higher than sum", async () => {
+            const [deployer, ...accounts] = await ethers.getSigners();    
+            const amount = ethers.parseUnits("1", 18);
+            await token.connect(deployer).approve(instance.getAddress(), ethers.MaxUint256 );
+            const users = accounts.map((acc) => ({
+                user: acc.address,
+                amount: amount,
+            }));
+            await expect(instance.MultiSendERC20Indirect(token.getAddress(), amount * BigInt(accounts.length + 1), users)).to.be.revertedWithCustomError(instance, "TotalMismatch").withArgs(true)
+        })
+
+        it("should revert ERC20 indirect transfer when total lower than sum", async () => {
+            const [deployer, ...accounts] = await ethers.getSigners();    
+            const amount = ethers.parseUnits("1", 18);
+            const total = amount * BigInt(accounts.length);
+            await token.connect(deployer).transfer(instance, total ); // manually sending tokens to contract to increase its balance
+            await token.connect(deployer).approve(instance.getAddress(), ethers.MaxUint256 );
+            const users = accounts.map((acc) => ({
+                user: acc.address,
+                amount: amount,
+            }));
+            await expect(instance.MultiSendERC20Indirect(token.getAddress(), amount * BigInt(accounts.length - 1), users)).to.be.revertedWithCustomError(instance, "TotalMismatch").withArgs(false)
+        })
+        
     })
 
     describe("MultiManageable", () => {
@@ -86,7 +107,7 @@ describe("MultiSender", () => {
             await instance.Pause()
             expect(await instance.paused()).to.be.true
             await expect(instance.MultiSendEth([])).to.be.revertedWith("Pausable: paused")
-            await expect(instance.MultiSendERC20Direct(token.getAddress(), 0, [])).to.be.revertedWith("Pausable: paused")
+            await expect(instance.MultiSendERC20Direct(token.getAddress(), [])).to.be.revertedWith("Pausable: paused")
             await expect(instance.MultiSendERC20Indirect(token.getAddress(), 0, [])).to.be.revertedWith("Pausable: paused")
             await instance.Unpause()
             expect(await instance.paused()).to.be.false
